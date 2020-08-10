caa6314c623917bc1d445c09e0134adaf63dc716
tvaidotas <tadas.vaidotas@gmail.com>
2020-08-10 11:53:35 +0100

    added a new class for the fizzbuzz exercise

--- src/main/java/com/qa/day2/FizzBuzz.java
+++ src/main/java/com/qa/day2/FizzBuzz.java
@@ -4,6 +4,11 @@ public class FizzBuzz {
 
     public static void main(String[] args) {
         System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 
 }
